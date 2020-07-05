6519065f8a1d607d363733b9fd31001fdb9e0fe5
Mason <lixq@weilaicheng.com>
2020-07-05 21:46:44 +0800

    week2 homework2

---
+++ week02/week02_homework02.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+import time
+
+try:
+    browser = webdriver.Chrome()
+
+    # browser.get('https://shimo.im/desktop')
+
+    browser.get('https://shimo.im/login?from=home')
+    time.sleep(1)
+
+    browser.find_elements_by_tag_name('input')[0].clear()
+    time.sleep(1)
+    browser.find_elements_by_tag_name('input')[0].send_keys("username") # fake username & password
+    time.sleep(2)
+
+    browser.find_elements_by_tag_name('input')[1].clear()
+    time.sleep(1)
+    browser.find_elements_by_tag_name('input')[1].send_keys("password")
+    time.sleep(2)
+
+    loginBtn = browser.find_element_by_xpath("//*[@id='root']/div/div[2]/div/div/div/div[2]/div/div/div[1]/button")
+    loginBtn.click()
+    time.sleep(5)
+
+except Exception as e:
+    print(e)
+finally:
+    browser.close()
